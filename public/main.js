1741f875c23eed9e6e64b927e611ec0fa97df1ac
MICHAEL J MELTON <meltonm83@gmail.com>
2016-06-30 10:34:58 -0700

    Stops drawing on mouseup

--- public/main.js
+++ public/main.js
@@ -13,16 +13,24 @@ $(function() {
             context.fill();
         };
 
+        var drawing = false;
+
         canvas = $('canvas');
         context = canvas[0].getContext('2d');
         canvas[0].width = canvas[0].offsetWidth;
         canvas[0].height = canvas[0].offsetHeight;
-        canvas.on('mousemove', function(event) {
-            var offset = canvas.offset();
-            var position = {x: event.pageX - offset.left,
-                        y: event.pageY - offset.top};
-            draw(position);
-            socket.emit('draw', position);
+        canvas.on('mousedown', function(event) {
+            drawing = true;
+        }).on('mousemove', function(event) {
+            if (drawing) {
+                var offset = canvas.offset();
+                var position = {x: event.pageX - offset.left,
+                                y: event.pageY - offset.top};
+                draw(position);
+                socket.emit('draw', position);
+            };
+        }).on('mouseup', function(event) {
+            drawing = false;
         });
         socket.on('draw', function(position) {
             draw(position);
